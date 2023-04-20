2ee3211eaed382fa6512d4f88a583da591d06bf4
Volodya <volodya.atoyan@list.ru>
2023-04-20 16:59:03 +0300

    add task1

---
+++ task1.java
@@ -0,0 +1,14 @@
+
+/**
+ * task1
+ */
+import java.util.Random;
+
+public class task1 {
+   int i = new Random().nextInt(2001);
+
+   public static void main(String[] args) {
+      task1 obj = new task1();
+      System.out.println(obj.i);
+   }
+}
